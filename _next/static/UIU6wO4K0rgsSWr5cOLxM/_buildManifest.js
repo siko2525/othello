74b2d3dffbafe74fcc6bbcb23c704cd6230591b7
siko2525 <siko2525@users.noreply.github.com>
2023-06-08 16:45:55 +0000

    deploy: 3273f13720b06781a5b03db59188417903c7f8ac

---
+++ _next/static/UIU6wO4K0rgsSWr5cOLxM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a3a32fa801e2718a.css","static/chunks/pages/index-77541509798c1491.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
