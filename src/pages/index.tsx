464104179047b55c5c33c4ea3b959db5ec2748ad
siko2525 <s1f102301820@iniad.org>
2023-06-14 14:21:07 +0900

    コミットメッセージ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,19 +7,48 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
     [0, 0, 0, 2, 1, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const directions = [
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [-1, -1],
+    [0, -1],
+    [1, -1],
+  ];
   const onClick = (x: number, y: number) => {
     console.log(x, y);
     const newBoard: number[][] = JSON.parse(JSON.stringify(board));
-    if (board[y + 1] !== undefined && board[y + 1][x] !== 0 && board[y + 1][x] !== turnColor) {
-      newBoard[y][x] = turnColor;
-      setTurnColor(-turnColor + 3);
+    // if (board[y + 1] !== undefined && board[y + 1][x] !== 0 && board[y + 1][x] !== turnColor) {
+    //   console.log(turnColor);
+    //   newBoard[y][x] = turnColor;
+    //   console.table(newBoard);
+    // }
+    for (const dir of directions) {
+      for (let i = 1; i < 8; i++) {
+        if (newBoard[y + i * dir[1]] === undefined) {
+          break;
+        } else if (newBoard[y + i * dir[1]][x + i * dir[0]] === turnColor && i !== 1) {
+          for (let a = 0; a < i; a++) {
+            newBoard[y + a * dir[1]][x + a * dir[0]] = turnColor;
+          }
+          setTurnColor(-turnColor + 3);
+          break;
+        } else if (newBoard[y + i * dir[1]][x + i * dir[0]] === turnColor) {
+          break;
+        } else if (newBoard[y + i * dir[1]][x + i * dir[0]] === 0) {
+          break;
+        }
+      }
     }
+
     setBoard(newBoard);
   };
   return (
@@ -30,13 +59,15 @@ const Home = () => {
             <div className={styles.cell} key={`${x}-${y}`} onClick={() => onClick(x, y)}>
               {color !== 0 && (
                 <div className={styles.stone} style={{ background: color === 1 ? `#000` : '#fff' }}>
+                  {/* <div className = {styles.empty}> {turncolor: }</div> */}
                   {' '}
-                </div>
+                  </div>
               )}
             </div>
           ))
         )}
       </div>
+      <div className = {styles.turn}>{turnColor=== 1 ? '黒のターン': '白のターン'}</div>
     </div>
   );
 };
