a932c8d30d89720da093d37f328096a68efa431f
siko2525 <s1f102301820@iniad.org>
2023-06-22 21:08:46 +0900

    ぶっ壊す２

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,7 +1,7 @@
 import styles from './cell.module.css';
 export const Cell = (props: { x: number; y: number; color: number; onClick: () => void }) => {
   return (
-    <div className={styles.line} onClick={props.onClick}>
+    <div className={styles.cell} onClick={props.onClick}>
       {props.color !== 0 && (
         <div className={styles.stone} style={{ background: props.color === 1 ? '#000' : '#fff' }} />
       )}
