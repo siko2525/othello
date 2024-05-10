fc2eee83b80e8444c7c9cea6b0075c7d56d28e7e
siko2525 <siko2525@users.noreply.github.com>
2024-05-10 14:39:54 +0000

    deploy: f9011fc201e7a3ba85faac0900b5f8a1ea138c5f

---
+++ _next/static/c49_jAxdbwocl9JSMofPC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1091b59c528893f8.css","static/chunks/pages/index-b945e6e56eaa288c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
